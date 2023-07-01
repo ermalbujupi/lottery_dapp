d7b0e7f402d760fa26ed18fe212b7bb63ffb034c
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-01 11:46:29 +0200

    Add contract functions

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
+/**
+ * @title A sample Raffle Contract
+ * @author Ermal Bujupaj
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
 contract Raffle {
-    constructor() {}
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() external payable {
+        require(msg.value >= i_entranceFee, "Not enough eth sent");
+    }
+
+    function pickWinner() public {}
+
+    /** Getter Functions */
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
