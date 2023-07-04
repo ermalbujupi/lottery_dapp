896e9021ec6d716369bc804e2ec57c991cffaf99
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-04 21:48:46 +0200

    Add Event to track when a player entered Raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -11,8 +11,10 @@ contract Raffle {
     error Raffle_NotEnoughEthSent();
 
     uint256 private immutable i_entranceFee;
-
     address payable[] private s_players;
+
+    //* Events */
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
@@ -23,6 +25,7 @@ contract Raffle {
             revert Raffle_NotEnoughEthSent();
         }
         s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender)
     }
 
     function pickWinner() public {}
