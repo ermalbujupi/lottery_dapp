46afae6ba54b451335002b1ace3c623b68f5714b
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-10 20:27:39 +0200

    Add VRF from Chainlinl

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,17 +10,37 @@ pragma solidity ^0.8.18;
 contract Raffle {
     error Raffle_NotEnoughEthSent();
 
+    /** State variables */
+    uint256 private constant REQUEST_CONFIRMATIONS = 3;
+    uint256 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval; // @dev Duration of the lottery in seconds
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
     uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
     //* Events */
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId
+        uint32 callbackGasLimit.
+    ) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
     }
 
@@ -40,6 +60,14 @@ contract Raffle {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gas lane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            numWords
+        );
     }
 
     /** Getter Functions */
