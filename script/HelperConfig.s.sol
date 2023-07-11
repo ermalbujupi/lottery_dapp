4b6278beb7f9fdbbe826d331892949356b5004e4
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-11 20:59:48 +0200

    Added HelperConfig script which will be used for creating Raffle in our deploy script

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+}
