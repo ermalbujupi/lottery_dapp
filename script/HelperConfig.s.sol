2ac594e899870fae26e4689b7acb9564c70762d6
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-12 08:00:53 +0200

    Added function to build configs for sepolia and anvil

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,4 +13,33 @@ contract HelperConfig is Script {
         uint64 subscriptionId;
         uint32 callbackGasLimit;
     }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // is sepolia
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // key hash in chainlink
+                subscriptionId: 0, // update this later with our sub id
+                callbackGasLimit: 500000 // 500,000 gas
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+    }
 }
