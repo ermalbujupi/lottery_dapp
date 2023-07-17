a7c9cf5d5ea2addf06c59c82dc90c62412d67c8a
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-17 20:31:39 +0200

    added testRaffleRecordsPlayerWhenTheyEnter test

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -49,4 +49,11 @@ contract RaffleTest is Test {
         vm.expectRevert(Raffle.Raffle_NotEnoughEthSent.selector);
         raffle.enterRaffle();
     }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        assert(playerRecorded == PLAYER);
+    }
 }
