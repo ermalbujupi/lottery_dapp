e579861e5828f8dc3639b59408be1342d0da22a6
Ermal Bujupaj <ermal.bj@gmail.com>
2023-07-17 18:49:26 +0200

    Add function to get raffle state

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -149,6 +149,10 @@ contract Raffle is VRFConsumerBaseV2 {
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
+
+    function getRaffleState() external view returns (RaffleState) {
+        return s_raffleState;
+    }
 }
 
 // CEI:
